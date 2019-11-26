3949a306c6547cd0e2b3c10732aeae104ff6dbb5
oonaosterberg <oonaos@metropolia.fi>
2019-11-26 19:36:51 +0200

    ruokalista toimii

---
+++ app.js
@@ -0,0 +1,37 @@
+
+/*
+const fetch = require('node-fetch');
+const fs = require('fs');
+
+
+let date = new Date();
+let day = date.getDate() +1;
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+let data = [];
+console.log(day, month, year)
+
+fetch('https://www.sodexo.fi/ruokalistat/output/daily_json/16365/'+year+'/'+month+'/'+day+'/fi')
+.then(function(response){
+  if(response.ok){
+    return response.json();
+  } else{
+    throw new Error('Response not ok');
+  }
+})
+.then(function(myJson) {
+  console.log(myJson.courses.length);
+  myJson.courses.forEach((t) => data.push({name: t.title_fi, price: t.price}));
+  console.log(data);
+  console.log(day, month, year);
+
+  fs.writeFile('ruoka.json', JSON.stringify(data), (err) => {
+    if(err) throw err;
+
+  });
+})
+.catch(function(e) {
+  console.log( e.message, data);
+
+});
+*/
---
+++ main.js
@@ -0,0 +1,48 @@
+const pad2 = (number) => {
+
+    return (number < 10 ? '0' : '') + number;
+
+};
+
+const date = new Date;
+const day = pad2(date.getDate());
+const month = pad2(date.getMonth() + 1);
+const year = pad2(date.getFullYear());
+const weekday = pad2(date.getDay());
+let lista = document.getElementById('ruokaLista');
+
+document.getElementById('kampuslista').addEventListener('click', function() {
+
+    lista.innerHTML = "";
+
+    let url = 'https://cors-anywhere.herokuapp.com/https://www.sodexo.fi/ruokalistat/output/daily_json/' +
+        + event.target.id + '/' + year + '/' + month + '/' + day + '/fi';
+
+    console.log(day);
+    console.log(month);
+    console.log(year);
+
+        fetch(url).then((response) => {
+            return response.json();
+        }).then((result) => {
+
+            const lunch = result.courses;
+            console.log(lunch[0]);
+
+            for (let i = 0; i < lunch.length; i++) {
+
+
+                lista.innerHTML += lunch[i].title_fi.bold() + ' / ' + lunch[i].title_en.bold() + ' ' + '<br>' + 'Hinta: ' + lunch[i].price + '<br>' +
+                    lunch[i].properties + "<br>" + "<br>";
+            }
+
+            if (weekday == 0 || weekday == 6) {
+                lista.innerHTML = 'Tämä ravintola ei tarjoile lounasta viikonloppuisin.';
+            }
+        })
+
+})
+
+
+
+
